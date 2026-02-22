c153a9f6cfcbe5a079913b17192db1ba2a446498
Venkata Sai Krishna Talupuri <saisaikrishna58@gmail.com>
2026-02-22 16:03:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ import os
 import csv
 from datetime import datetime
 from utils import load_class_indices, decode_prediction, validate_class_mapping, is_healthy_prediction
+import os
 import tensorflow as tf
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 tf.config.set_visible_devices([], 'GPU')
-import tensorflow as tf
-
 # Optional OpenCV for basic leaf detection/segmentation
 try:
     import cv2
